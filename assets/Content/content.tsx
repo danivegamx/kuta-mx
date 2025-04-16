92f8d0c95321f400ab07edacc0890fd9c646e35b
Dafne <dafne@quantumstudios.dev>
2025-04-16 09:13:05 -0600

    Added a content tsx file and another image

---
+++ assets/Content/content.tsx
@@ -0,0 +1,5 @@
+export const content = {
+  landingS1Content: `Creemos que cada mascota merece una segunda oportunidad y un hogar donde sea amada. Detrás de cada mirada hay una historia, y con tu ayuda, puede tener un nuevo comienzo.\n
+Trabajamos junto a refugios y rescatistas para conectar a perros y gatos con personas dispuestas a darles una vida llena de cariño. Adoptar no solo cambia la vida de una mascota, también transforma la tuya.\n
+Nuestra misión es simple pero poderosa: <strong>ayudar a que más animales encuentren un hogar lleno de amor.</strong>`,
+};
