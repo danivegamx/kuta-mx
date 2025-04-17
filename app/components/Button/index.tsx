8adb8949c52cb862e99d2e33cde352cc53de72c6
Dafne <dafne@quantumstudios.dev>
2025-04-17 08:33:54 -0600

    Added a hover effect to the adoption apply button

--- app/components/Button/index.tsx
+++ app/components/Button/index.tsx
@@ -1,18 +1,32 @@
-
-import './styles.sass';
+import "./styles.sass";
 
 export default function Button({ data }: any) {
-  const { title, type, metadata: { url, action, color, sin_borde } } = data;
-  return (<>
-    {
-      url ? (
-        <a className={`${sin_borde ? ` text-white`: 'bg-transparent border'} flex flex-1 rounded-md py-2 justify-center`} 
-        style={sin_borde ? { backgroundColor: color } : {backgroundColor: '#ffffff', borderColor: color}} href={url}>{title}</a>
+  const {
+    title,
+    type,
+    metadata: { url, action, color, sin_borde },
+  } = data;
+  return (
+    <>
+      {url ? (
+        <a
+          className={`${
+            sin_borde
+              ? ` text-white`
+              : "bg-transparent border hover:bg-gradient-to-r from-purple to-violet-900 hover:text-white hover:border-transparent transition duration-300 ease-in-out"
+          } flex flex-1 rounded-md py-2 justify-center`}
+          style={
+            sin_borde ? { backgroundColor: color } : { borderColor: color }
+          }
+          href={url}
+        >
+          {title}
+        </a>
       ) : (
-        <button onClick={() => action()} className='w-full' >
+        <button onClick={() => action()} className="w-full">
           {title}
         </button>
-      )
-    }
-  </>);
+      )}
+    </>
+  );
 }
