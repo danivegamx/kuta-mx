5c095a90eb50b03f82df128f360bbd69a3bfa028
Dafne <dafne@quantumstudios.dev>
2025-04-24 16:33:04 -0600

    Worked on the happy endings section. Mostly styles

--- app/components/Happy Endings/happyEndings.tsx
+++ app/components/Happy Endings/happyEndings.tsx
@@ -1,14 +1,31 @@
 import Image from "next/image";
 import startEmphasis from "../../../assets/star-emphasis.png";
+import emmaBefore from "../../../assets/emma-before.jpeg";
+import emmaAfter from "../../../assets/emma-after.jpeg";
+import freyaBefore from "../../../assets/freya-before.jpeg";
+import freyaAfter from "../../../assets/freya-after.jpeg";
+
+// const images = [
+//   "https://images.wagwalkingweb.com/media/daily_wag/blog_articles/hero/1722953201.3050609/fluffy-dogs-hero-1-1.jpg",
+//   "https://cdn.outsideonline.com/wp-content/uploads/2023/03/Funny_Dog_S.jpg",
+//   "https://www.rawmate.com/cdn/shop/articles/dogs_with_purpose_puppy_720x.jpg",
+//   "https://d2zp5xs5cp8zlg.cloudfront.net/image-66559-800.jpg",
+//   "https://www.dogstrust.ie/images/800x600/assets/2022-07/Buster_germanshepherd_outdoors_leeds_dogstrust.jpg",
+//   "https://www.rspcansw.org.au/wp-content/uploads/2025/01/Robbie-and-Coco-016-1-1024x1024.jpg",
+//   "https://pethelpful.com/.image/w_3840,q_auto:good,c_fill,ar_4:3/MTk2NTM5MzQxNzI2NTU4MTc1/five-ferocious-dog-breeds-your-family-must-be-afraid-of-2.jpg"
+// ];
 
 const images = [
-  "https://images.wagwalkingweb.com/media/daily_wag/blog_articles/hero/1722953201.3050609/fluffy-dogs-hero-1-1.jpg",
-  "https://cdn.outsideonline.com/wp-content/uploads/2023/03/Funny_Dog_S.jpg",
-  "https://www.rawmate.com/cdn/shop/articles/dogs_with_purpose_puppy_720x.jpg",
-  "https://d2zp5xs5cp8zlg.cloudfront.net/image-66559-800.jpg",
-  "https://www.dogstrust.ie/images/800x600/assets/2022-07/Buster_germanshepherd_outdoors_leeds_dogstrust.jpg",
-  "https://www.rspcansw.org.au/wp-content/uploads/2025/01/Robbie-and-Coco-016-1-1024x1024.jpg",
-  "https://pethelpful.com/.image/w_3840,q_auto:good,c_fill,ar_4:3/MTk2NTM5MzQxNzI2NTU4MTc1/five-ferocious-dog-breeds-your-family-must-be-afraid-of-2.jpg"
+  {
+    name: "Emma",
+    before: emmaBefore,
+    after: emmaAfter,
+  },
+  {
+    name: "Freya",
+    before: freyaBefore,
+    after: freyaAfter,
+  },
 ];
 
 export default function HappyEndings() {
@@ -30,12 +47,44 @@ export default function HappyEndings() {
             className="scale-x-[-1] w-8 h-12"
           ></Image>
         </div>
-        <div className="overflow-hidden whitespace-nowrap bg-white py-4">
+        <div className="overflow-hidden whitespace-nowrap py-4">
           <div className="w-max animate-marquee flex flex-row">
             {[...images, ...images].map((image: any, index: number) => {
               return (
-                <div key={index} className="w-60 h-60 rounded-lg bg-cover bg-center bg-no-repeat shrink-0 mx-2" style={{backgroundImage: `url(${image})`}}> 
+                <div
+                  key={index}
+                  className="rounded-lg p-6 bg-white border border-slate-300 flex flex-col justify-center gap-y-6 mx-4"
+                >
+                  <h4 className="font-caveat text-[22px] text-center text-slate-800">
+                    {image.name}
+                  </h4>
+                  <div className="flex flex-row gap-x-4">
+                    <div className="flex flex-col gap-y-4">
+                        <div className="w-48 h-[250px] relative overflow-hidden rounded-md">
+                          <Image
+                            src={image.before}
+                            alt="Before Image"
+                            fill
+                            className="object-cover"
+                          />
+                        </div>
+                        <h5 className="font-caveat text-slate-800 text-xl text-center">Antes</h5>
+                    </div>
+                    <div className="flex flex-col gap-y-4">
+                        <div className="w-48 h-[250px] relative overflow-hidden rounded-md">
+                          <Image
+                            src={image.after}
+                            alt="After Image"
+                            fill
+                            className="object-cover"
+                          />
+                        </div>
+                        <h5 className="font-caveat text-slate-800 text-xl text-center">Despues</h5>
+                    </div>
+                  </div>
                 </div>
+                // <div key={index} className="w-60 h-60 rounded-lg bg-cover bg-center bg-no-repeat shrink-0 mx-2" style={{backgroundImage: `url(${image})`}}>
+                // </div>
               );
             })}
           </div>
